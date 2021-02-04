cd5dab5aba17c75a820c0027db5fc080ece25106
Anurag Kotwal <anuragjamliya85@gmail.com>
2021-02-04 15:42:52 +0530

    Assignment 2

---
+++ floatmultiply.java
@@ -0,0 +1,9 @@
+public class floatmultiply {
+    public static void main(String[] args) {
+        float mul,a,b;
+        a=Float.parseFloat(args[0]);
+        b=Float.parseFloat(args[1]);
+        mul = a*b;
+        System.out.println("Multiplication of Floating Numbers: "+mul);
+    }
+}
